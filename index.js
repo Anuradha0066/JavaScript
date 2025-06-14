1a96b0b2a31ffe86122134f9e32e414b74f8f156
Anuradha0066 <singhanuradha0066@gmail.com>
2025-06-14 21:19:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+  // let h1= document.getElementById("one")
+  // h1.style.color='red'
+  // h1.style.backgroundColor='black'
+  // h1.innerText="hiiii"
+  // console.log(h1);
+  
+//      let h1= document.getElementsByClassName("two")
+//   //  h1[0].style.backgroundColor='red'
+
+// for(let i of h1){
+//   i.style.color='red'
+// }
+
+    // console.log(h1);
+    //     console.log(h2);
+
+   // let h1=document.querySelector("#one")//id
+//let h1=document.querySelector(".two")
+// let h1=document.querySelectorAll(".two")
+// console.log(h1);
+
+// let a=document.querySelector('a')
+// console.log(a.getAttribute('href'));
+// a.setAttribute("href","http://flipkart.com")
+
+const h1= document.createElement('h1')
+const div=document.querySelector('div')
+h1.innerText='hello'
+div.append(h1)
+console.log(div);
+
