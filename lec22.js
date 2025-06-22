b08d89f49f6d4eb8fb34da23bf6f0cae9a4392b9
Anuradha Singh <singhanuradha0066@gmail.com>
2025-06-22 14:38:32 +0530

    code

---
+++ lec22.js
@@ -0,0 +1,27 @@
+// let inp= document.querySelector("input")
+// let h1=document.querySelector("h1")
+// inp.addEventListener('input',(e)=>{
+//     // console.log(e);
+//     // console.log(e.target.value);
+//     h1.innerText=e.target.value
+    
+// })
+
+let h3=document.querySelector('h3')
+let h4=document.querySelector('h4')
+let h5=document.querySelector('h5')
+
+let form=document.querySelector("form")
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+     console.log(form[0].value+" "+form[1].value+" "+form[2].value);
+     
+})
+
+let data={
+    .form[0].value,
+    number.
+}
+
+// localStorage.setItem('id',324)
+// console.log(localStorage.setItem("id"));
