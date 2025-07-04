34706ba97d3233bd10ad2e085b086a0a19896de7
Anuradha Singh <singhanuradha0066@gmail.com>
2025-07-04 22:34:40 +0530

    Jquery

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-  // let h1= document.getElementById("one")
-  // h1.style.color='red'
-  // h1.style.backgroundColor='black'
-  // h1.innerText="hiiii"
-  // console.log(h1);
-  
-//      let h1= document.getElementsByClassName("two")
-//   //  h1[0].style.backgroundColor='red'
+// let h2=document.querySelector("h2")
+// h2.innerText
 
-// for(let i of h1){
-//   i.style.color='red'
-// }
+// $("h2").css({
+//     color:"red",
+//     border:"1px solid green",
+// })
+// console.log($("h2").text("hiii"));
 
-    // console.log(h1);
-    //     console.log(h2);
+$("button").click(()=>{
+ console.log("hehehe");
+ $("h2").fadeOut()
+})
 
-   // let h1=document.querySelector("#one")//id
-//let h1=document.querySelector(".two")
-// let h1=document.querySelectorAll(".two")
-// console.log(h1);
-
-// let a=document.querySelector('a')
-// console.log(a.getAttribute('href'));
-// a.setAttribute("href","http://flipkart.com")
-
-const h1= document.createElement('h1')
-const div=document.querySelector('div')
-h1.innerText='hello'
-div.append(h1)
-console.log(div);
-
+$("#in").click(()=>{
+    $("h2").fadeIn()
+})
