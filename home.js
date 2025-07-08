f962ca8c03de7e0398ca27d76159cd88ad7a226e
Anuradha Singh <singhanuradha0066@gmail.com>
2025-07-08 22:53:12 +0530

    Spread_Operator

---
+++ home.js
@@ -0,0 +1,15 @@
+// export let user="hello"
+let user="hello"
+
+export default function share(a,b){
+    console.log(a+b);
+    
+}
+
+ 
+
+export{
+    user,share
+}
+
+//export default let hello ="hiiii"
